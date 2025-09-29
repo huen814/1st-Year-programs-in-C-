f81c2820e4a8d75e62e4d345ba62e829dd646f58
huen <ashtontehcreator@gmail.com>
2025-09-29 22:18:34 +0800

    Add files via upload

---
+++ secondsToMilitarytime.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int getHours(int time1);
+int getMinutes(int time1);
+void displayTime(int time1);
+
+int main(){
+    int time1;
+
+    printf("Enter time: ");
+    scanf("%d",&time1);
+
+    displayTime(time1);
+
+    return 0;
+}
+int getHours(int time1){
+    return time1 / 100;
+}
+int getMinutes(int time1){
+    return time1 % 100;
+}
+void displayTime(int time1){
+    int hours = getHours(time1);
+    if (hours < 0 || hours > 23) {
+        printf("Invalid time :");
+    }
+    int minutes = getMinutes(time1);
+    if (hours < 0 || hours > 59) {
+        printf("Invalid time");
+    }
+    else {
+        printf("%d : %d",hours,minutes);
+    }
+}

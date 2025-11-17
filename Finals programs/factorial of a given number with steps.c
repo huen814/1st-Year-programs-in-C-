7a86cf13b2f543e9d6bfd39509df6639de7ef9bf
huen <ashtontehcreator@gmail.com>
2025-11-17 10:45:28 +0800

    Enhance factorial program with error handling
    Added error handling for negative input and clarified output for factorial calculation.

--- Finals programs/factorial of a given number with steps.c
+++ Finals programs/factorial of a given number with steps.c
@@ -13,18 +13,25 @@ int main(){
     
     printf("Enter a number: ");
     scanf("%d",&n);
-    
+    if(n < 0){
+        printf("Error: Factorial of a negative number doesn't exist.");
+    }
+    else if(n == 0){
+        printf("Factorial of 0 is 1");
+        printf("\n0! = 1");
+    }
+    else{
     int fact = factorial(n);
     
-    printf("Factorial of %d is %d\n",n,fact);
-    
-    printf("%d! = ",n);
+        printf("Factorial of %d is %d\n",n,fact);
+        
+        printf("%d! = ",n);
     for(int i = 1; i <= n; i++){
         printf("%d",i);
-    if(i < n){
-        printf(" * ");
+        if(i < n){
+            printf(" * ");
+        }
     }
 }
-
     return 0;
 }

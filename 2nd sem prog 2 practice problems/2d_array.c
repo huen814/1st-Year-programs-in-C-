324b6f78848195507a018b241d12cb723dd7da87
huen <ashtontehcreator@gmail.com>
2026-02-17 21:36:06 +0800

    Add 2D array example with corrected output

---
+++ 2nd sem prog 2 practice problems/2d_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+   
+    int array[3][3]={ {1,2,3},
+                    {4,5,6},
+                    {7,8,9}
+                    };
+                   
+    printf("%d ",array[0][0]);
+    printf("%d ",array[0][1]);
+    printf("%d \n",array[0][2]);
+    printf("%d ",array[0][3]);
+    printf("%d ",array[0][4]);
+    printf("%d \n",array[0][5]);
+    printf("%d ",array[0][6]);
+    printf("%d ",array[0][7]);
+    printf("%d ",array[0][8]);
+   
+    return 0;
+}
